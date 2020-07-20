030db415baced24ab52068b3d750d4d0c29b7d4a
Ilya Ballet <anonimus33@protonmail.com>
2020-07-20 14:19:24 +0300

    updated OS-specific.hpp

--- src/OS-specific.hpp
+++ src/OS-specific.hpp
@@ -1,1 +1,13 @@
 #pragma once
+
+#ifdef LINUX
+    #include "../dependencies/SFML/linux/include/SFML/Graphics.hpp"
+#endif
+
+#ifdef WINDOWS
+    #include "../dependencies/SFML/windows/include/SFML/Graphics.hpp"
+#endif
+
+#ifdef MACOS
+    #include "../dependencies/SFML/macOS/include/SFML/Graphics.hpp"
+#endif
